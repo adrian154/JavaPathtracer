a256769a21bd41a5347c389eedd4e1eff402e79e
adrian154 <94568a@gmail.com>
2021-07-29 08:50:40 -0700

    fixed cylinder isect from inside

--- src/com/JavaPathtracer/geometry/Cylinder.java
+++ src/com/JavaPathtracer/geometry/Cylinder.java
@@ -1,7 +1,5 @@
 package com.JavaPathtracer.geometry;
 
-import javax.lang.model.util.Elements.Origin;
-
 import com.JavaPathtracer.Raytracer;
 
 public class Cylinder implements Shape {
@@ -37,37 +35,45 @@ public class Cylinder implements Shape {
 		
 		double discrim = b * b - 4 * a * c;
 		if(discrim < 0) return null;
-		
+
+		// t1 will always be closer
 		discrim = Math.sqrt(discrim);
-		
 		double t1 = (-b - discrim) / (2 * a);
 		double t2 = (-b + discrim) / (2 * a);
 
+		// both behind
 		if(t1 < Raytracer.EPSILON && t2 < Raytracer.EPSILON) return null;
 		
 		Vector hitLocal;
+		double t;
 		Vector hit1 = o.plus(d.times(t1));
 		Vector hit2 = o.plus(d.times(t2));
 		
+		if(t1 < Raytracer.EPSILON && t2 < Raytracer.EPSILON) return null;
+		
 		if(t1 < Raytracer.EPSILON) {
-			hitLocal = hit2;
+			hitLocal = hit2; t = t2;
 		} else if(t2 < Raytracer.EPSILON) {
-			hitLocal = hit1;
+			hitLocal = hit1; t = t1;
 		} else {
 			if(hit1.y > length || hit1.y < 0) hit1 = null;
 			if(hit2.y > length || hit2.y < 0) hit2 = null;
-			hitLocal = hit1 == null ? hit2 : hit1;
+			if(hit1 == null) {
+				hitLocal = hit2; t = t2;
+			} else {
+				hitLocal = hit1; t = t1;
+			}
 		}
 		
 		// transform back normal
-		if(hitLocal == null) return null;
+		if(hitLocal == null || hitLocal.y < 0 || hitLocal.y > length) return null;
 		Vector normalLocal = new Vector(hitLocal.x, 0, hitLocal.z).normalize();
 		
 		Vector point = Vector.localToWorldCoords(hitLocal, bvx, direction, bvz);
 		Vector normal = Vector.localToWorldCoords(normalLocal, bvx, direction, bvz);
 		
 		// TODO: texture mapping for cylinder
-		return new Hit(ray, point, normal, t1, new Vector(0.5, 0.5, 0.0));
+		return new Hit(ray, point, normal, t, new Vector(0.5, 0.5, 0.0));
 		
 	}
 	
