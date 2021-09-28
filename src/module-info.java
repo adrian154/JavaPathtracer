556cb3764cbc1e1dfe25f93c06237148e8737c5e
adrian <94568a@gmail.com>
2021-09-28 13:25:14 -0700

    module file (no idea what it does though)

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module javapathtracer {
+	exports com.JavaPathtracer.geometry;
+	exports com.JavaPathtracer.scene;
+	exports com.JavaPathtracer.cameras;
+	exports com.JavaHDR;
+	exports com.JavaPathtracer.renderer;
+	exports com.JavaPathtracer.material;
+	exports com.JavaPathtracer.geometry.csg;
+	exports com.JavaPathtracer.geometry.mesh;
+	exports com.JavaPathtracer.tonemapping;
+	exports com.JavaPathtracer;
+
+	requires java.desktop;
+}
